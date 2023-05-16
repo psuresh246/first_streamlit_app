f4fa3f0871abeac9624dadbe27c632dd0e557e57
psuresh246 <psuresh246@gmail.com>
2023-05-16 15:46:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit;
 streamlit.title('first streamlit app');
+streamlit.header('Diner menu');
+streamlit.text('1. chicken burger');
+streamlit.text('2. cheese burger');
+streamlit.text('3. 6" sub');
