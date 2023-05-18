15aadf17263c63cb5cfc382f9ea4ee55aa332c7d
psuresh246 <psuresh246@gmail.com>
2023-05-18 09:08:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # build a datatable using dataframe.
 streamlit.dataframe(fruityvice_normalized);
 
+streamlit.header("Fruityvice Fruit Advice");
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 streamlit.stop();
 #snowflake connector
 
