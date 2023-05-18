21102a17b14a5a589ab4cd1f46c9a4ff0a0f86da
psuresh246 <psuresh246@gmail.com>
2023-05-18 09:32:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,11 @@ def add_fruit(_fruit_name):
   with my_cnx.cursor() as my_cur:
      my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('"+_fruit_name+"')");
      return "thanks for adding "+_fruit_name;
-  
-add_my_fruit = streamlit.text_input("What fruit would you like to add?");
-streamlit.write(add_fruit(add_my_fruit));
+
+# add a button to load the fruit.
+if streamlit.button("Add Fruit to List"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]);
+  add_my_fruit = streamlit.text_input("What fruit would you like to add?");
+  streamlit.write(add_fruit(add_my_fruit));
 
 
